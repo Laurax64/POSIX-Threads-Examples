9073bae3d0e53552cce8c728f9afa0bfd3aa6a8b
Laura Voetzsch <56238822+Laurax64@users.noreply.github.com>
2024-07-25 14:15:34 +0200

    Update pthread_mutex_init.c

--- pthread_mutex_init.c
+++ pthread_mutex_init.c
@@ -8,21 +8,18 @@ void *PrintThreadId (void *threadid)
 {
   long tid = (long) threadid;
   printf ("tid: %ld\n", tid);
-  // Unlock the mutex after printing both t and tid with the same value
   pthread_mutex_unlock (&print_mutex);
 }
 
 int main (int argc, char *argv[])
 {
-  // Initialize the mutex
+  // Initialize the mutex, to ensure that the mutex is in a valid, known state before using it.
   pthread_mutex_init (&print_mutex, NULL);
   pthread_t threads[5];
   for (long t = 0; t < 5; t++)
 	{
-	  // Lock the mutex before printing
 	  pthread_mutex_lock (&print_mutex);
 	  printf ("t: %ld\n", t);
 	  pthread_create (&threads[t], NULL, PrintThreadId, (void *) t);
-
 	}
 }
