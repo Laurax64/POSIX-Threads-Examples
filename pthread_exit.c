e90358024048c3ed33d34ea7d36ffbc9f7f8b431
Laura Voetzsch <56238822+Laurax64@users.noreply.github.com>
2024-07-25 16:08:37 +0200

    Update pthread_exit.c

--- pthread_exit.c
+++ pthread_exit.c
@@ -1,23 +1,18 @@
 #include <pthread.h>
 #include <stdio.h>
 
-void *PrintThreadId (void *threadid)
-{
-  long tid = (long) threadid;
-  printf ("tid: %ld\n", tid);
+void *PrintThreadId (void *threadid) {
+	long tid = (long) threadid;
+	printf ("tid: %ld\n", tid);
 }
 
-int main (int argc, char *argv[])
-{
-  pthread_t threads[5];
-  for (long t = 0; t < 5; t++)
-	{
-	  if (t > 1)
-		{
-		  pthread_exit (NULL);
+int main (int argc, char *argv[]) {
+	pthread_t threads[5];
+	for (long t = 0; t < 5; t++) {
+		if (t > 1) {
+			pthread_exit (NULL);
 		}
-	  printf ("t: %ld\n", t);
-	  pthread_create (&threads[t], NULL, PrintThreadId, (void *) t);
-
+		printf ("t: %ld\n", t);
+		pthread_create (&threads[t], NULL, PrintThreadId, (void *) t);
 	}
 }
