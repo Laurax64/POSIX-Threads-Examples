2ebd667ff74236dff760505a753412118a44db11
Laura Voetzsch <56238822+Laurax64@users.noreply.github.com>
2024-07-25 11:20:30 +0200

    Add files via upload

---
+++ pthread_mutex_init.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+
+// Mutex for synchronizing print output
+pthread_mutex_t print_mutex;
+
+void *PrintThreadId (void *threadid)
+{
+  long tid = (long) threadid;
+  printf ("tid: %ld\n", tid);
+  // Unlock the mutex after printing both t and tid with the same value
+  pthread_mutex_unlock (&print_mutex);
+}
+
+int main (int argc, char *argv[])
+{
+  // Initialize the mutex
+  pthread_mutex_init (&print_mutex, NULL);
+  pthread_t threads[5];
+  for (long t = 0; t < 5; t++)
+	{
+
+	  // Lock the mutex before printing
+	  pthread_mutex_lock (&print_mutex);
+	  printf ("t: %ld\n", t);
+	  pthread_create (&threads[t], NULL, PrintThreadId, (void *) t);
+
+	}
+}
